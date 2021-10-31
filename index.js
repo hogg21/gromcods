155fabfaeea9120a573edde78e39c464b60ae407
hogg21 <89107241+hogg21@users.noreply.github.com>
2021-10-31 12:30:08 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Hello, world!');
+function run() {
+    var a = 0;
+  
+    if (a == 1) {
+      return c;
+    }
+  
+    for (let i = 1; i < 10; i++) {
+      console.log(i);
+    }
+  
+    return (d = a);
+  }
+  
+  run();
